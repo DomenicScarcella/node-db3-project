999d708aebf705e4063cb013b7bcb37956eed189
Domenic Scarcella <ls41513@yahoo.com>
2021-04-15 00:06:00 -0400

    3 of 4 queries.sql done

--- queries.sql
+++ queries.sql
@@ -2,8 +2,38 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select productname, categoryname
+from product as p
+join category as c
+on p.categoryid = c.id
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+I have no idea how to cut off the results by date
+
+select od.orderid, s.companyname, o.orderdate
+from [Order] as o
+left join shipper as s
+on o.shipvia = s.id
+left join orderdetail as od
+on o.id = od.orderid
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select orderid, productname, quantity
+from orderdetail as od
+join product as p
+on od.productid = p.id
+where orderid = 10251
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select o.id, c.companyname, e.lastname
+from [Order] as o
+join employee as e
+on o.employeeid = e.id
+join customer as c
+on o.customerid = c.id
